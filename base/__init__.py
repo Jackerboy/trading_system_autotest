d9e64ad3bb3c6a6ef9ce196c968edd1af72b74fe
Shaobangzhu <andyluloveusc@gmail.com>
2022-08-17 15:50:55 -0700

    自动化测试框架中基于PO模式的代码结构

--- base/__init__.py
+++ base/__init__.py
@@ -1,4 +1,4 @@
 #! /usr/bin/python3
 # coding=utf-8
-# @Time: 8/17/2022 12:12 AM
+# @Time: 8/17/2022 1:55 PM
 # @Author: Chaoran Lu
---
+++ logs/__init__.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python3
+# coding=utf-8
+# @Time: 8/17/2022 1:56 PM
+# @Author: Chaoran Lu
