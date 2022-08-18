d089331b2ced7bfe474abeb2337a101a43d309a3
Shaobangzhu <andyluloveusc@gmail.com>
2022-08-17 18:54:44 -0700

    yaml

--- common/tools.py
+++ common/tools.py
@@ -4,11 +4,31 @@
 # @Author: Chaoran Lu
 
 import datetime
+import os
 
 
 def get_current_time():
     return datetime.datetime.now()
 
 
+def get_project_path():
+    """
+    获取项目的绝对路径
+    :return:
+    """
+    project_name = "trading_system_autotest"
+    file_path = os.path.dirname(__file__)
+    return file_path[:file_path.find(project_name) + len(project_name)]
+
+
+def sep(path, add_sep_before=False, add_sep_after=False):
+    all_path = os.sep.join(path)
+    if add_sep_before:
+        all_path = os.sep + all_path
+    if add_sep_after:
+        all_path = all_path + os.sep
+    return all_path
+
+
 if __name__ == '__main__':
-    print(get_current_time())
+    sep(["config", "environment.yaml"], add_sep_before=True)
---
+++ common/yaml_config.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+# coding=utf-8
+# @Time: 8/17/2022 5:36 PM
+# @Author: Chaoran Lu
+import yaml
+from common.tools import get_project_path, sep
+
+
+class GetConf:
+    def __init__(self):
+        with open(get_project_path()+sep(["config", "environment.yaml"], add_sep_before=True), "r",
+                  encoding="utf-8") as env_file:
+            self.env = yaml.load(env_file, Loader=yaml.FullLoader)
+
+    def get_username_password(self):
+        return self.env["username"], self.env["password"]
+
+
+if __name__ == '__main__':
+    print(GetConf().get_username_password())
