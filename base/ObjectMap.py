4d4230e61825ba0024b02867798e6ca8bfc04570
Shaobangzhu <andyluloveusc@gmail.com>
2022-08-21 00:26:24 -0700

    单个元素获取

---
+++ base/ObjectMap.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3
+# coding=utf-8
+# @Time: 8/20/2022 11:45 PM
+# @Author: Chaoran Lu
+import time
+
+from selenium.common.exceptions import ElementNotVisibleException
+
+class ObjectMap:
+
+    def element_get(self, driver, locate_type, locator_expression, timeout=10, must_be_visible=False):
+        """
+        单个元素获取
+        :param driver: 浏览器驱动
+        :param locate_type: 定位方式类型
+        :param locator_expression: 定位表达式
+        :param timeout: 超时时间
+        :param must_be_visible: 元素是否必须可见，True是必须可见，False是默认值
+        :return: 返回的元素
+        """
+        # 开始时间
+        start_ms = time.time() * 1000
+        # 设置结束时间
+        stop_ms = start_ms + (timeout * 1000)
+        for x in range(int(timeout * 10)):
+            # 查找元素
+            try:
+                element = driver.find_element(by=locate_type, value=locator_expression)
+                # 如果元素不是必须可见的
+                if not must_be_visible:
+                    return element
+                # 如果元素不是必须可见的,则需要先判断元素是否可见
+                else:
+                    if element.is_displayed():
+                        return element
+                    else:
+                        raise Exception()
+            except Exception:
+                now_ms = time.time() * 1000
+                if now_ms >= stop_ms:
+                    break
+                pass
+            time.sleep(0.1)
+            raise ElementNotVisibleException("元素定位失败, 定位方式：" + locate_type + " 定位表达式: " + locator_expression)
