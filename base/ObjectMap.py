c9ef3c3183e0807cdb22308be87df878f3ade821
Shaobangzhu <andyluloveusc@gmail.com>
2022-08-22 18:34:12 -0700

    跳转地址方法

--- base/ObjectMap.py
+++ base/ObjectMap.py
@@ -6,8 +6,12 @@ import time
 
 from selenium.common.exceptions import ElementNotVisibleException, WebDriverException
 
+from common.yaml_config import GetConf
 
 class ObjectMap:
+
+    # 获取基准地址
+    url = GetConf().get_url()
 
     def element_get(self, driver, locate_type, locator_expression, timeout=10, must_be_visible=False):
         """
@@ -134,3 +138,43 @@ class ObjectMap:
             raise ElementNotVisibleException("元素没有出现, 定位方式: " + locate_type + "\n定位表达式:" + locator_expression)
         else:
             pass
+
+    def element_to_url(self,
+                       driver,
+                       url,
+                       locate_type_disappear=None,
+                       locator_expression_disappear=None,
+                       locate_type_appear=None,
+                       locator_expression_appear=None
+                       ):
+        """
+        跳转地址
+        :param driver: 浏览器驱动
+        :param url: 跳转的地址
+        :param locate_type_disappear: 等待页面元素消失的定位方式
+        :param locator_expression_disappear: 等待页面元素消失的定位表达式
+        :param locate_type_appear: 等待页面元素出现的定位方式
+        :param locator_expression_appear: 等待页面元素出现的定位表达式
+        :return:
+        """
+
+        try:
+            driver.get(self.url + url)
+            # 等待页面元素都加载完成
+            self.wait_for_ready_state_complete(driver)
+            # 跳转地址和后,等待元素消失
+            self.element_disappear(
+                driver,
+                locate_type_disappear,
+                locator_expression_disappear
+            )
+            # 跳转地址后,等待元素出现
+            self.element_appear(
+                driver,
+                locate_type_appear,
+                locator_expression_appear
+            )
+        except Exception as e:
+            print("跳转地址出现异常, 异常原因:%s" % e)
+            return False
+        return True
--- common/yaml_config.py
+++ common/yaml_config.py
@@ -8,12 +8,15 @@ from common.tools import get_project_path, sep
 
 class GetConf:
     def __init__(self):
-        with open(get_project_path()+sep(["config", "environment.yaml"], add_sep_before=True), "r",
+        with open(get_project_path() + sep(["config", "environment.yaml"], add_sep_before=True), "r",
                   encoding="utf-8") as env_file:
             self.env = yaml.load(env_file, Loader=yaml.FullLoader)
 
     def get_username_password(self):
         return self.env["username"], self.env["password"]
+
+    def get_url(self):
+        return self.env["url"]
 
 
 if __name__ == '__main__':
